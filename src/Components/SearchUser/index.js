abda5c2d8f0ce9793dda7031ecb63e441c71f8ca
“millman97” <thomas.millington@mail.com>
2022-06-22 09:47:27 +0100

    add initial components

---
+++ src/Components/SearchUser/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+const SearchUser = () => {
+    const [queryInput, setQueryInput] = useState('');
+
+    let navigate = useNavigate();
+    const routeChange = (username) => {
+        let path = `/${username}`;
+        navigate(path);
+    }
+
+    const handleInput = (e) => {
+        setQueryInput(e.target.value)
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        routeChange(queryInput)
+        setQueryInput('');
+    }
+
+    return (
+        <>
+            <form onSubmit={handleFormSubmit}>
+                <input type='text'
+                    id='search'
+                    placeholder="Enter your GitHub username..."
+                    value={queryInput}
+                    style={{ display: 'inline-block', width: '60vw', padding: '25px 20px 25px 20px', fontSize: '30px', borderRadius: '50px', marginTop: '25vh' }}
+                    onChange={handleInput}
+                />
+                <input type='submit' value='Submit' style={{ display: 'none' }} />
+            </form>
+        </>
+    )
+}
+
+export default SearchUser;
